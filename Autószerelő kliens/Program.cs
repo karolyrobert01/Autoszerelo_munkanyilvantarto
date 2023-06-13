36459e294510b994ee3bd7b87b0e23a4a9b99e43
Károly Róbert <karolyrobert01@gmail.com>
2023-06-13 21:19:38 +0200

    1st commit

--- Autószerelő kliens/Program.cs
+++ Autószerelő kliens/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+//commit test #KR
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
